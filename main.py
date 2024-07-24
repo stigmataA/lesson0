b06431d9401d990011e0f04fe48e7d3dc3581f5b
stigmataA <r.muhutdinov@gmail.com>
2024-07-24 14:37:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print(''"1st program"'')
+print((9 * 5) ** 0,5)
+print('"2nd program"')
+print(9.99 > 9.98 and 1000 != 1000.1)
+print('"3rd program"')
+print(((5678 % 1000) // 10) + ((1234 % 1000) // 10))
+print('"4th program"')
+print((13.42 // 1) == 13 or (42.13 // 1) == 42)
